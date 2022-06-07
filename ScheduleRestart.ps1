22aada3ba1bf2fea7af605de6eb5c07d74e4e4a1
Javier Bravo <javierbravorivera@gmail.com>
2022-06-06 20:47:23 -0400

    Add files via upload

---
+++ ScheduleRestart.ps1
@@ -0,0 +1,27 @@
+#create action
+$taskAction = New-ScheduledTaskAction `
+    -Execute "shutdown" `
+    -Argument "/r"
+
+$taskAction
+
+$targetDate = (Get-Date).AddDays(1)
+$targetDate = $targetDate.Date
+
+#create trigger                                     
+$taskTrigger = New-ScheduledTaskTrigger -Once -At $targetDate
+$taskTrigger
+
+
+#name of scheduled task
+$taskName = "ServerRestart"
+
+#description of scheduled task
+$description = "Scheduled server restart."
+
+#Register the scheduled task
+Register-ScheduledTask `
+    -TaskName $taskName `
+    -Action $taskAction `
+    -Trigger $taskTrigger `
+    -Description $description 
