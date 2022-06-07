5eb31ab2cb139ed4f1ec12972094ca3da296137c
Javier Bravo <javierbravorivera@gmail.com>
2022-06-06 20:38:01 -0400

    Add files via upload

---
+++ HardDriveSpace.ps1
@@ -0,0 +1,28 @@
+﻿#Script checks each drive mapped to computer and writes to host if there are drives with over 90% utilization
+
+$computername = (Get-Content Env:COMPUTERNAME)
+$volumes = (Get-Volume)
+$listOfDrives = @()
+
+#$volumes.GetType().SizeRemaining
+
+foreach($volume in $volumes)
+{
+    $spaceRemaining = [single]($volume.SizeRemaining)
+    $spaceAlloted = [single]($volume.Size)
+    $spaceUnusedPercent = ($spaceRemaining / $spaceAlloted)
+
+    if($spaceUnusedPercent -lt .10)
+    {
+
+       $listOfDrives += [string]($volume.DriveLetter)
+
+    }
+
+}
+
+if(($listOfDrives.Count -ge 1))
+{
+Write-Host ("Drives with over 90% utilization on " + $computername + ":")
+$listOfDrives
+}
